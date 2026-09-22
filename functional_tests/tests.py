aa187d5427590285387be2d5c024a128457e6665
Vitória de Oliveira Cavalcante <Vitoria34195546@edu.df.senac.br>
2026-09-21 21:20:11 -0300

    Melhoria 2: Removendo Sleeps para Evitar Nao Determinismo

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,10 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -30,13 +41,11 @@ class NewVisitorTest(LiveServerTestCase):
 
         inputbox.send_keys('Estudar testes funcionais')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
 
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Estudar testes de unidade')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Estudar testes funcionais')
-        self.check_for_row_in_list_table('2: Estudar testes de unidade')
+        self.wait_for_row_in_list_table('1: Estudar testes funcionais')
+        self.wait_for_row_in_list_table('2: Estudar testes de unidade')
